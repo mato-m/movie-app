411a3585fd1013d04869bcb31430794e8e6cbb8b
Mato Martinović <martinovic.mato99@gmail.com>
2023-11-13 03:03:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -31,5 +31,5 @@ app.use(express.static(path.join(__dirname, "dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Server started on port " + port));
